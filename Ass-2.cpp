2bdd33abbf03e6e701b684d6c3a61f2e7cf69800
Abhishek SIngh <abhishekmithileshsingh1@gmail.com>
2022-01-11 11:56:04 +0530

    Print odd NUmber in Range

--- Ass-2.cpp
+++ Ass-2.cpp
@@ -3,16 +3,42 @@
 
 #include <iostream>
 
+
+
+#include <iostream>
+
+//int main()
+//{
+//    int number{};
+//    std::cout << "please enter maximum limit value to print odd number = " << std::endl;
+//    std::cin >> number;
+//    int i1n{ 1 };
+//    std::cout << "print odd number from 1 to " << number << std::endl;
+//    while (i1n <= number) {
+//        std::cout << i1n << std::endl;
+//        i1n = i1n + 2;
+//    }
+//    return 0;
+//}
+ 
+
+/*Print odd in Range */
 int main()
 {
     int number{};
     std::cout << "please enter maximum limit value to print odd number = " << std::endl;
     std::cin >> number;
-    int i1n{ 1 };
-    std::cout << "print odd number from 1 to " << number << std::endl;
+    int i1n{ };
+    std::cout << "Please enter Minimum limit :" ;
+    std::cin >> i1n;
+    std::cout << "print odd number from " << i1n << " to " << number << std::endl;
     while (i1n <= number) {
-        std::cout << i1n << std::endl;
-        i1n = i1n + 2;
+        if (i1n % 2 != 0) {
+            std::cout << i1n << std::endl;
+            
+        }
+        i1n = i1n + 1;
+       
     }
     return 0;
 }
